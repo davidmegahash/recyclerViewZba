aea2077e655483b48447f21a3e6674f07a52cd0c
david.mazo <david.mazo@globant.com>
2019-08-23 17:25:23 -0500

    ArrayList of Items created

---
+++ app/src/main/java/com/globant/training/recyclerviewzba/FakeActivity.kt
@@ -0,0 +1,12 @@
+package com.globant.training.recyclerviewzba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FakeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake)
+    }
+}
--- app/src/main/java/com/globant/training/recyclerviewzba/MainActivity.kt
+++ app/src/main/java/com/globant/training/recyclerviewzba/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.globant.training.recyclerviewzba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.globant.training.recyclerviewzba.model.Item
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CustomAdapter.AdapterEvents {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager = StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL)
+
+        val fakeIntent = Intent(applicationContext,FakeActivity::class.java)
+        val items = ArrayList<Item>()
+
+        items.add(Item(1,"diceRoller","Today could be your lucky day!","dice_roller_icon",fakeIntent))
+        items.add(Item(2,"diceRoller2","Today could be your lucky day!","about_me_icon",fakeIntent))
+        items.add(Item(3,"diceRoller3","Today could be your lucky day!","trivia_icon",fakeIntent))
+        items.add(Item(4,"diceRoller4","Today could be your lucky day!","more_icon",fakeIntent))
+
+        val adapter = CustomAdapter(items,this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onItemClicked(item:Item){
+        startActivity(item.intent)
     }
 }
