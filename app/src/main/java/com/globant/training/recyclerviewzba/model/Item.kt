804653b62f53d17a0fc9454864ce9ed64ae1a39e
david.mazo <david.mazo@globant.com>
2019-08-23 14:34:06 -0500

    Data class item has been added

---
+++ app/src/main/java/com/globant/training/recyclerviewzba/model/Item.kt
@@ -0,0 +1,5 @@
+package com.globant.training.recyclerviewzba.model
+
+import android.content.Intent
+
+data class Item (val id:Int, val title: String, val subTitle:String, val image:String, val intent: Intent)
